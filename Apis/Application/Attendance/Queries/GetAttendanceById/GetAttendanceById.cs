c2c39b9eb2ab5883fe1a2d400d28e201164d60ef
Vo Thuong Truong Nhon <vothuongtruongnhon2002@gmail.com>
2023-05-10 14:00:35 +0700

    crud attendance

--- Apis/Application/Attendance/Queries/GetAttendanceById/GetAttendanceById.cs
+++ Apis/Application/Attendance/Queries/GetAttendanceById/GetAttendanceById.cs
@@ -1,9 +1,9 @@
-﻿using Application.Attendences.DTO;
+﻿using Application.Attendances.DTOs;
 using Application.Common.Exceptions;
 using AutoMapper;
 using MediatR;
 
-namespace Application.Attendences.Queries.GetAttendanceById
+namespace Application.Attendances.Queries.GetAttendanceById
 {
     public record GetAttendanceByIdQuery(int id) : IRequest<AttendanceDTO>;
 

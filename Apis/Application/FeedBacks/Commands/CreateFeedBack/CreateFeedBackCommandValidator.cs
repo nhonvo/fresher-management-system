70522091034d7488e2b0b9487dcfa91ff57e65f9
MANT\Admin <thanhngnam@gmail.com>
2023-05-12 08:48:30 +0700

    Nam - remove unnecessary usings and add Validator

---
+++ Apis/Application/FeedBacks/Commands/CreateFeedBack/CreateFeedBackCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.FeedBacks.Commands.CreateFeedBack
+{
+    public class CreateFeedBackCommandValidator : AbstractValidator<CreateFeedBackCommand>
+    {
+        public CreateFeedBackCommandValidator()
+        {
+            RuleFor(x => x.Rating).InclusiveBetween(1, 5);
+            RuleFor(x => x.Comment).MaximumLength(500);
+            RuleFor(x => x.StudentId).GreaterThan(0);
+        }
+    }
+
+}

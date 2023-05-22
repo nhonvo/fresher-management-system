452f16a05385e1ed7ca204e928217da6fb081afe
Vo Thuong Truong Nhon <vothuongtruongnhon2002@gmail.com>
2023-05-22 10:00:38 +0700

    update user(role and status)

---
+++ Apis/Application/Users/Commands/UpdateUser/UpdateUserCommand.cs
@@ -0,0 +1,45 @@
+﻿using Application.Common.Exceptions;
+using Application.Students.DTO;
+using Application.Users.DTO;
+using AutoMapper;
+using Domain.Entities;
+using Domain.Enums;
+using MediatR;
+
+namespace Application.Student.Commands.UpdateUser
+{
+    public record UpdateUserCommand : IRequest<UserDTO>
+    {
+        public int Id { get; set; }
+        public UserRoleType Role { get; set; }
+        public UserStatus Status { get; set; }
+    }
+    public class UpdateUserHandler : IRequestHandler<UpdateUserCommand, UserDTO>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public UpdateUserHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        public async Task<UserDTO> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _unitOfWork.UserRepository.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (user is null)
+            {
+                throw new NotFoundException("Student not found");
+            }
+
+            user.Role = request.Role;
+            user.Status = request.Status;
+
+            await _unitOfWork.ExecuteTransactionAsync(() =>
+            {
+                _unitOfWork.UserRepository.Update(user);
+            });
+            var result = _mapper.Map<UserDTO>(user);
+            return result;
+        }
+    }
+}
--- Apis/WebAPI/Controllers/UserController.cs
+++ Apis/WebAPI/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 using Apis.Domain.Enums;
 using Application.Commons;
+using Application.Student.Commands.UpdateUser;
 using Application.Users.Commands.ImportUsersCSV;
 using Application.Users.DTO;
 using Application.Users.GetProfile.Queries;
@@ -20,14 +21,20 @@ namespace WebAPI.Controllers
         {
             _mediator = mediator;
         }
+
         [HttpGet]
         [Authorize(Roles = "SuperAdmin")]
-        public async Task<Pagination<UserDTO>> GetAsync(
+        public async Task<Pagination<UserContainIdDTO>> GetAsync(
             string? keyword,
             SortType sortType = SortType.Ascending,
             int pageIndex = 0,
             int pageSize = 10)
             => await _mediator.Send(new GetUserQuery(keyword, pageIndex, pageSize, sortType));
+
+        [HttpPut]
+        [Authorize(Roles = "SuperAdmin")]
+        public async Task<UserDTO> Put(UpdateUserCommand request)
+            => await _mediator.Send(request);
 
         [HttpGet("Profile")]
         [Authorize]

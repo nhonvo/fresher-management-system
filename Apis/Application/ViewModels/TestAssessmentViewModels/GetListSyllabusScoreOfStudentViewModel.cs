35ecaea9ae6d2ab9defa1cc96e0667af4cf84162
Đạt Trần <dattm283@gmail.com>
2023-05-23 09:05:09 +0000

    Dattm42 add average class of student

--- Apis/Application/ViewModels/TestAssessmentViewModels/GetListSyllabusScoreOfStudentViewModel.cs
+++ Apis/Application/ViewModels/TestAssessmentViewModels/GetListSyllabusScoreOfStudentViewModel.cs
@@ -6,6 +6,7 @@
         public int SyllabusId { get; set; }
         public string SyllabusName { get; set; }
         public int TrainingClassId { get; set; }
+        public float FinalScheme { get; set; }
         public List<GetStudentTestScoreViewModel> ListAssessment { get; set; }
     }
 }
